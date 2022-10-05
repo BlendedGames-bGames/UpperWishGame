426ed3f0ee3356f13feb3336738836c20264b836
Benja Cruz <benjamin.cruz.v@usach.cl>
2022-10-05 20:47:23 -0300

    Se añade menú de inicio

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+
+    private bool juegoPausado=false;
+
+    public void Pausa(){
+        juegoPausado=true;
+        Time.timeScale=0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.G)){
+            if(juegoPausado){
+                Reanudar();
+            }
+            else{
+                Pausa();
+            }
+        }
+    }
+
+     public void Reanudar(){
+        juegoPausado=false;
+
+        Time.timeScale=1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+}
