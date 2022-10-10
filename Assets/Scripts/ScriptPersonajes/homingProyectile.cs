e3f65d3b06ad54bf6d7064018da99e01c90c27bb
overthinkin-mochi <64170546+overthinkin-mochi@users.noreply.github.com>
2022-10-10 00:38:23 -0300

    Proyectiles y script de armas para el personaje

--- Assets/Scripts/ScriptPersonajes/homingProyectile.cs
+++ Assets/Scripts/ScriptPersonajes/homingProyectile.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class homingProyectile : MonoBehaviour
-{
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class homingProyectile : MonoBehaviour{
+
+    public Transform target;
+    public float speed = 10f;
+
+    private Rigidbody2D proyectile;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        proyectile = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        Vector2 direction = (Vector2)target.position - proyectile.position;
+
+        direction.Normalize();
+        float rotation = Vector3.Cross(direction, transform.up).z;
+
+        proyectile.angularVelocity = -rotation * speed;
+        proyectile.velocity = transform.up * speed;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        Destroy(gameObject);
+    }
+
 }
