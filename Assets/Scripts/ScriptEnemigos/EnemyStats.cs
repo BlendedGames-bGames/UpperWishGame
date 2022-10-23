1f16edf358b5eb3db38f8daeffba4e90179e0866
Francisco Moreno <francisco.moreno.l@usach.cl>
2022-10-23 02:35:39 -0300

    se creo un scriptable object para las estadisticas de los enemigos
    a demas se creo una carpeta para contener mas ordenadamente los scripts de los enemigos

---
+++ Assets/Scripts/ScriptEnemigos/EnemyStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : ScriptableObject
+{
+    public int hp;//vida del enemigo
+    public int damage;//daño del enemigo
+    public int speed;//velocidad del enemigo
+}
