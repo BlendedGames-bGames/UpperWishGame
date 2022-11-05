0c40ce3e93b437ceba1126b6a478aee1d8492a1a
Andres-Araya <andres.araya@usach.cl>
2022-11-05 05:30:32 -0300

    Algoritmo de generación aleatoria sin base de inicio y fin de niveles
    Creación de paleta con recursos gráficos de Fantasy Cave, se hace uso del script "GeneratePoint" para crear puntos en los que se toman escenarios prefabricados  de pruebas para la generación aleatoria de niveles. Pruebas hechas sobre los ejemplos de parallax ya que solo es su fase de experimentación. Listo para ser implementado de forma concisa.

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -28,8 +28,8 @@ public class Parallax : MonoBehaviour
 
         pos.x -= realVelocity * Time.fixedDeltaTime;
 
-        if (pos.x <= -25)
-            pos.x = 10;
+        if (pos.x <= -15)
+            pos.x = 5;
 
         transform.position = pos;
     }
