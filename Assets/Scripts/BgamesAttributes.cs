8f15747a1c25d7a224cafbc9c16325fb3ace58e0
overthinkin-mochi <64170546+overthinkin-mochi@users.noreply.github.com>
2022-12-11 18:19:27 -0300

    Creacion de formato Puntos bGames

--- Assets/Scripts/BgamesAttributes.cs
+++ Assets/Scripts/BgamesAttributes.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BgamesAtritbutes {
+// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+public class BgamesAttritbutes {
     public int id_attributes { get; set; }
     public string name { get; set; }
     public int data { get; set; }
--- Assets/Scripts/BgamesPoint.cs
+++ Assets/Scripts/BgamesPoint.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,23 +18,31 @@ public class BgamesPoint : MonoBehaviour
 
     IEnumerator GetPoints() {
          Debug.Log("Se consiguieron los atributos");
-        using(UnityWebRequest www = UnityWebRequest.Get($"localhost:3001/player_all_attributes/{idPlayer}"))
-        {
-        yield return www.SendWebRequest();
-        Debug.Log("llego");
-        if(www.isNetworkError || www.isHttpError) {
-            Debug.Log("holi");
-            Debug.Log(www.error);
-        }
-        else {
-            // Show results as text
-            Debug.Log("holo");
-            Debug.Log(www.downloadHandler.text);
- 
-            // Or retrieve results as binary data
-            byte[] results = www.downloadHandler.data;
+        using (UnityWebRequest www = UnityWebRequest.Get($"localhost:3001/player_all_attributes/{idPlayer}"))
+            {
+            yield return www.SendWebRequest();
+            Debug.Log("llego");
+            if (www.isNetworkError || www.isHttpError) {
+                Debug.Log("Error de conexion");
+                Debug.Log(www.error);
+                }
+            else {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+                }
+            // Se transforma el json en un objeto bGamesAtributes
+            List<BgamesAttritbutes> attributeList = new List<BgamesAttritbutes>();
+            attributeList = JsonConvert.DeserializeObject<List<BgamesAttritbutes>>(www.downloadHandler.text);
+
+            Debug.Log(attributeList[0].name + ": " + attributeList[0].data);
+            Debug.Log(attributeList[1].name + ": " + attributeList[1].data);
+            Debug.Log(attributeList[2].name + ": " + attributeList[2].data);
+            Debug.Log(attributeList[3].name + ": " + attributeList[3].data);
+            Debug.Log(attributeList[4].name + ": " + attributeList[4].data);
             }
-        }
     }
     
     
